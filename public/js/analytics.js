8c01940542f194c8a36af67019c1a1a3df9d94f2
ccsaposs <32380359+ccsaposs@users.noreply.github.com>
2020-07-25 00:24:49 -0700

    Add QR code referral analytics

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
   let db = firebase.firestore();
+  // Check query string for QR code referral
+  // Each QR code we distribute (e.g. on a poster) has a unique ID
+  const qrId = (new URL(document.location)).searchParams.get('qr');
+  // Remove query string
+  window.history.replaceState(null, null, window.location.pathname);
+  if (qrId != null) {
+    // Track QR code referral
+
+    const now = firebase.firestore.Timestamp.now().toMillis();
+    // Saves date instead of full timestamp to respect user privacy
+    // QR codes are often associated with location data, and saving a full timestamp might allow users to be indirectly identified.
+    const date = now - now % 86400000;
+    db.collection('qr_referrals').add({
+      page_path: window.location.pathname,
+      qr_id: qrId,
+      date: firebase.firestore.Timestamp.fromMillis(date),
+    });
+  }
   // Track page load
   db.collection('page_loads').add({
     page_path: window.location.pathname,
